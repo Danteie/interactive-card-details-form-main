b88f2e48a816b9b80baa120ee49c606525c38b93
Danteie <aleksandar.djuratovic93@gmail.com>
2022-09-13 10:30:08 +0200

    addin logic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
 import './App.css';
 import Card from './Commponent/Card'
 import back from './Assets/bg-card-back.png'
+import { useState } from 'react';
+
 
 
 function App() {
+
+  const [name, setName] = useState();
+  const [number, setNumber] = useState(0);
+  const [month, setMonth] = useState();
+  const [year, setYear] = useState();
+  const [cvc, setCvc] = useState();
+
+
+  function cardholder(event){
+      setName(name => name = event.target.value)
+      console.log(name);
+  }
+
+  function cardNumber(event){
+    setNumber(number => number = event.target.value)
+    console.log(number);
+  }
+
+  function cardMonth(event){
+    setMonth(month => month = event.target.value)
+    console.log(month);
+  }
+
+  function cardYear(event){
+    setYear(year => year = event.target.value)
+    console.log(year);
+  }
+
+  function cardCvc(event){
+    setCvc(cvc => cvc = event.target.value)
+    console.log(cvc);
+  }
+
+
+
+
   return (
     <div className="App">
       {/* left part of the content */}
       <div className='cards'>
           <div className='card'>
-            <Card />
+            <Card name={name} number={number} month={month} year={year} cvc={cvc}/>
             <img  src={back} alt='slika2' className='back-card'/>
           </div>
       </div>
@@ -17,20 +55,20 @@ function App() {
       <div className='login'>
         <form>
           <h1>CARDHOLDER NAME</h1>
-          <input type="text" placeholder='e.g Jane Appleseed'/>
+          <input type="text" placeholder='e.g Jane Appleseed' onChange={cardholder}/>
           <h1>CARD NUMBER</h1>
-          <input type="text" placeholder='e.g 1234 5678 9123 0000'/>
+          <input type="text" placeholder='e.g 1234 5678 9123 0000' onChange={cardNumber}/>
           <div className='date-field'>
             <div className='dateone'>
               <h1>EXP. DATE(MM/YY)</h1>
               <div className='inputmmyy'>
-                <input type="text" className='small' placeholder='MM'/>
-                <input type="text" className='small' placeholder='YY'/>
+                <input type="text" className='small' placeholder='MM' onChange={cardMonth}/>
+                <input type="text" className='small' placeholder='YY' onChange={cardYear}/>
               </div>
             </div>
             <div className='datetwo'>
             <h1>cvc</h1>
-            <input type="text" placeholder='e.g. 123'/>
+            <input type="text" placeholder='e.g. 123' onChange={cardCvc}/>
             </div>
           </div>
           <button>Condirm</button>
--- src/Commponent/Card.js
+++ src/Commponent/Card.js
@@ -1,15 +1,15 @@
 import './Card.css'
 import logo from'../Assets/card-logo.svg'
 
-export default function Card() {
+export default function Card({name,number,month,year,cvc}) {
   return (
     <div className="front-card">
         <img src={logo} className='logo' alt='logo'/>
         <div className='data-content'>
-          <h1>0000 0000 0000 0000</h1>
+          <h1>{number ? number : '0000 0000 00000 0000'}</h1>
           <div className='date'>
-              <h1>name surname</h1>
-              <h1>09/00</h1>
+              <h1>{name ? name : 'name surname'}</h1>
+              <h1>{month ? month: '09'}/{year ? year: '00'}</h1>
           </div>
         </div>
 
