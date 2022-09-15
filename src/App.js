0050a5dfad04c293ae12a5aeb81be53e1081b3b5
Danteie <aleksandar.djuratovic93@gmail.com>
2022-09-15 12:19:54 +0200

    addin confirm page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {useState,useEffect} from 'react';
 
 function App() {
 
+  const [correct, setCorrect] = useState(() => true);
   const [name, setName] = useState();
   const [number, setNumber] = useState(0);
   const [month, setMonth] = useState(0);
@@ -14,19 +15,52 @@ function App() {
   const [cvc, setCvc] = useState(0);
 
   
+  function isCorrect(){
+      if(correct){
+        return(
+          <form>
+          <h1>CARDHOLDER NAME</h1>
+          <input type="text" placeholder='e.g Jane Appleseed' onChange={cardholder} required/>
+          <h1>CARD NUMBER</h1>
+          <input type="text" placeholder='e.g 1234 5678 9123 0000' onChange={cardNumber} id='number' maxLength="16"/>
+          <h2>Wrotn format, numbers only</h2>
+          <div className='date-field'>
+            <div className='dateone'>
+              <h1>EXP. DATE(MM/YY)</h1>
+              <div className='inputmmyy'>
+                <input type="text" className='small' placeholder='MM' onChange={cardMonth} id='month' maxLength="2" required/>
+                <input type="text" className='small' placeholder='YY' onChange={cardYear} id='year' maxLength="2" required/>
+              </div>
+            </div>
+            <div className='datetwo'>
+            <h1>CVC</h1>
+            <input type="text" placeholder='e.g. 123' onChange={cardCvc} id='cvc' maxLength="3" required/>
+            </div>
+          </div>
+          <button type="submit" onClick={() => setCorrect(false)}>Condirm</button>
+        </form>
+        )
+      }else{
+        return <h1>Bravo</h1>
+      }
+  }
 
   function cardholder(event){
       setName(name => name = event.target.value)
   }
 
   function isNumber(number,id){
+    if(document.getElementById(id) != null){
       if(!isNaN(number)){
         document.getElementById(id).style.border = '1px solid rgba(0, 0, 0, 0.2)'
         document.getElementById(id).style.color = 'black'
+
       }else{
         document.getElementById(id).style.border = '2px solid red'
         document.getElementById(id).style.color = 'red'
       } 
+    }
+      
   }
 
   function cardNumber(event){
@@ -70,28 +104,8 @@ useEffect(()=>{
       </div>
       {/* rigth part of the content */}
       <div className='login'>
-        <form>
-          <h1>CARDHOLDER NAME</h1>
-          <input type="text" placeholder='e.g Jane Appleseed' onChange={cardholder}/>
-          <h1>CARD NUMBER</h1>
-          <input type="text" placeholder='e.g 1234 5678 9123 0000' onChange={cardNumber} id='number' maxLength="16"/>
-          <div className='date-field'>
-            <div className='dateone'>
-              <h1>EXP. DATE(MM/YY)</h1>
-              <div className='inputmmyy'>
-                <input type="text" className='small' placeholder='MM' onChange={cardMonth} id='month' maxLength="2"/>
-                <input type="text" className='small' placeholder='YY' onChange={cardYear} id='year' maxLength="2"/>
-              </div>
-            </div>
-            <div className='datetwo'>
-            <h1>CVC</h1>
-            <input type="text" placeholder='e.g. 123' onChange={cardCvc} id='cvc' maxLength="3"/>
-            </div>
-          </div>
-          <button>Condirm</button>
-        </form>
+        {isCorrect()}
       </div>
-
     </div>
   );
 }
